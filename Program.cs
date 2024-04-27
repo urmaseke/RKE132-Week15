f690237ca54184d09fdf50c600396d5a0fd039be
Urmas Õunapu. <urmas.ounapuu@tktk.ee>
2024-04-27 05:46:46 +0300

    add week15 code

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+List<Movie> mymovies =  new List<Movie>();
+string[] data = GetDataFromMyFile();
+
+
+foreach (string line  in data)
+{
+    string[] tempArray = line.Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie= new Movie(tempArray[0], tempArray[2]);
+    mymovies.Add(newMovie); 
+}
+
+foreach (Movie movie in mymovies)
+{
+    Console.WriteLine($"One of my favorite movies {movie.Title} was released in {movie.Year}.");
+}
+
+
+
+
+static string[] GetDataFromMyFile()
+{
+    string filepath = @"C:\Users\urmas\OneDrive\Documents\movies.txt";
+    return File.ReadAllLines(filepath);
+}
+
+static void ReadDataFromArray(string[] somearray)
+{
+    foreach (string line in somearray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Movie
+{
+    string title;
+    string year;
+
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+
+    
+
+    public Movie(string _title, string _year)
+    {
+        title = _title;
+        year = _year;
+    }
+}
